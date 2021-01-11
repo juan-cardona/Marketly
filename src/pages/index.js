7cf8621b4a4959d0a8ec8fdbd1d25a6866f566bf
grupodog <grupodog@gmail.com>
2021-01-11 14:38:31 -0600

    change name to 🐶

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default () => (
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            Grupo DOG
+          🐶
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
             Ayudamos a Líderes Méxicanos a acelerar la inovacion de Productos
